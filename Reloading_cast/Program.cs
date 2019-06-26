913836e1808a280bd37ece5dc08657ba10a0a39a
OlegStetsula <olegstetsula@gmail.com>
2019-06-26 16:00:46 +0300

    Added changes to Main Program

--- Reloading_cast/Program.cs
+++ Reloading_cast/Program.cs
@@ -6,11 +6,11 @@ namespace Reloading_cast
     {
         static void Main(string[] args)
         {
-            Time d = new Time(4,6);                     // 14760
+            Time d = new Time(4,15,20,200);                     // 15320.200
             TimeSpan ts = d;
             ts.Show();
 
-            TimeSpan times = new TimeSpan(3855);        // 1 : 4 : 15 . 0
+            TimeSpan times = new TimeSpan(55245);        // 15 : 20 : 45.0
             Time t2 = (Time)times;
             t2.Show();
 
